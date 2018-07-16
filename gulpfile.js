cd9f4c9d2136661cfd52a29f2d03a0ba14b54c03
cdl193 <clee5@uwhealth.org>
2018-07-16 18:41:26 -0500

    Clean up typography documentation

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,34 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const sassdoc = require('sassdoc');
+const browserSync = require('browser-sync').create();
 const series = gulp.series;
+const plumber = require('gulp-plumber');
 
-gulp.task('sass', (done) => {
+gulp.task('sass', function() {
     return gulp
-        .src(['./tests/*.scss'])
-        .pipe(sass({
-            sourceComments: false
-        }))
+        .src('./tests/test.scss')
+        .pipe(plumber())
+        .pipe(sass.sync()
+            .on('error', (err) => {
+                console.log(err);
+            })
+        )
+        .pipe(plumber.stop())
         .pipe(gulp.dest('./tests'))
-        .on('error', done)
-        .on('end', done);
+
 });
 
 gulp.task('docs', (done) => {
     const sdConfig = require('./.sassdoc.js');
-    return gulp
+    gulp
         .src('./**/*.scss')
-        .pipe(sassdoc(sdConfig))
-        .on('end', done)
-        .on('error', done)
+        .pipe(sassdoc(sdConfig)
+            .on('end', done))
+        .on('error', done);
 });
 
-gulp.task('watch', () => {
-    const browserSync = require('browser-sync').create();
-
+gulp.task('serve', (done) => {
     browserSync.init({
         files: ['./test/**/*.css', './test/**/*.html', './docs/**/*'],
         watch: true,
@@ -35,7 +38,11 @@ gulp.task('watch', () => {
         }
     });
 
-    gulp.watch(['./**/*.scss'], series('sass', 'docs'));
+    done();
 });
 
-gulp.task('default', series('sass', 'docs'));
+gulp.task('watch', gulp.parallel('serve', function watch(done) {
+    gulp.watch(['./**/*.scss', '!./node_modules/'], { ignoreInitial: false }, series('docs', 'sass'));
+}));
+
+gulp.task('default', series('docs', 'sass'));
